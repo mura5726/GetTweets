7a1e59b5d39b013ec44ee5b7a40232b85c4519d8
MMS <mura5726@gmail.com>
2018-01-15 02:40:45 +0900

    iroiro

--- Rcode/imi.R
+++ Rcode/imi.R
@@ -10,3 +10,32 @@ Obama.cleaned = removePunctuation(Obama.lower)
 
 #評判判定
 install.packages(c("tidytext", "dplyr"), dependencies = T) 
+library(tidytext)
+library(dplyr)
+
+#ポジティブな言葉を抽出する
+positive = filter(get_sentiments("nrc"), sentiment == "positive")
+Obama.positive = Obama.cleaned %in% positive$word #データの重なりをチェック
+Obama.positive.2 = grep("TRUE", Obama.cleaned %in% positive$word) #grepで位置を確認
+length(Obama.positive.2)
+Obama.cleaned[Obama.positive]
+
+#ネガティブな言葉を抽出する
+negative = filter(get_sentiments("nrc"), sentiment == "negative")
+Obama.negative = Obama.cleaned %in% negative$word #データの重なりをチェック
+Obama.negative.2 = grep("TRUE", Obama.cleaned %in% negative$word) #grepで位置を確認
+length(Obama.negative.2)
+Obama.cleaned[Obama.negative]
+
+#sentence
+#install package
+install.packages("devtools", dependencies = T)
+devtools::install_github("sfeuerriegel/SentimentAnalysis")
+library(SentimentAnalysis)
+library(NLP)
+Obama.2 = scan("C:/Users/mura5/Documents/GetTweets/SJIS_20170621/Data/Obama.txt",what = "char", sep = "\n", quiet = T)
+sentiment = analyzeSentiment(Obama.2)
+convertToDirection(sentiment$SentimentGI)
+table(convertToDirection(sentiment$SentimentGI))
+
+#日本語で評判分析を行うためのパッケージは存在しない？？？
---
+++ Rcode/jap_posi_nega.R
@@ -0,0 +1,27 @@
+# install.packages("plotrix")
+library(plotrix)
+
+library (RMeCab)
+# 単語感情極性表(Semantic Orientations of Words)を読み込みます
+sowdic <- read.table("http://www.lr.pi.titech.ac.jp/~takamura/pubs/pn_ja.dic",sep=":",col.names=c("term","kana","pos","value"),colClasses=c("character","character","factor","numeric"),fileEncoding="Shift_JIS")
+# 名詞＋品詞で複数の候補がある場合は平均値を採用します
+sowdic2 <- aggregate(value~term+pos,sowdic,mean)
+# ポジティブとネガティブを調整します(より傾向を出すため)
+sowdic2$value <- sowdic2$value + 0.6
+# 形態素解析の結果から頻度表を作成します
+war2007 <- RMeCabFreq("C:/Users/mura5/Documents/GetTweets/SJIS_20170621/Data/jap_pn.txt")
+
+# 単語感情極性表に含まれるものを抽出します
+war2007 <- subset(war2007,Term %in% sowdic2$term)
+# 単語感情極性表の属性をマージします
+war2007 <- merge(war2007,sowdic2,by.x=c("Term","Info1"),by.y=c("term","pos"))
+# キーワード毎にスコア算出します
+score2007 <- war2007[4:(ncol(war2007)-1)]*war2007$value
+# 描画用データを作成します
+war2007 <-c(sum(score2007 > 0),sum(score2007 < 0))
+# 描画用ラベルを作成します
+lbls <- c("positive", "negative")
+lbls <- paste(lbls, war2007, sep="：")
+# 三次元円グラフを描画します
+pie3D(war2007, radius=0.9, labels=lbls, explode=0.1, main="2007年 週報ネガポジ度")
+
